6265edbc3e824d27976f8b0bc89e88029b0711a9
Jaynielilie <santosjaymee984@gmail.com>
2025-11-27 16:46:30 +0800

    Add files via upload

---
+++ Fashion/includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    &copy; <?php echo date ("Y"); ?> Jaymee J. Santos Cyb-201 The Fashion Outlet. All rights reserved.
+</footer>
---
+++ Fashion/includes/header.php
@@ -0,0 +1,11 @@
+
+
+<h1>The Fashion Outlet</h1>
+<nav>
+    <a href="FashionOutlet_if_Else.php">If-Else</a> |
+    <a href="FashionOutlet_Switch.php">Switch</a> |
+    <a href="FashionOutlet_Loop.php">Loop</a> |
+    <a href="FashionOutlet_include.php">Include</a> |
+    <a href="FashionOutlet_Match.php">Match</a> |
+    <a href="FashionOutlet_While.php">While</a>|
+</nav> 
